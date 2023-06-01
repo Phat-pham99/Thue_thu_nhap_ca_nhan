dcee1944530b0af04e74556da07c49dc93c55186
Phat-pham99 <hphat99@gmail.com>
2023-06-01 23:13:35 +0700

    Update script + .md file

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Thue_thu_nhap_ca_nhan
+# thuế thu nhập cá nhân
+
 Script Python dùng để tính thuế thu nhập cá nhân. Chỉ cần nhập vào lương Gross + số người phụ thuộc
+
+```
+$ python thue_thu_nhap_ca_nhan.py
+Bạn có thể nhập số tiền bằng phân cách bằng dấu ',' , ví dụ : 10,000,000
+~ ---------- ~
+Nhập lương Gross:20,000,000
+Nhập số người phụ thuộc :0
+-----------------------------------
+Bảo hiểm cần đóng là 2,100,000 Đồng
+lương net là 17,900,000 Đồng
+Thu nhập tính thuế là 6,900,000 Đồng
+Vậy thuế thu nhập cá nhân bạn cần đóng là : 440,000 Đồng
+-----------------------------------
+Sau khi đóng thuế TNCN, bạn còn lại 17,460,000 Đồng
+```
+Bạn chỉ cần nhập vào :
+```
+Nhập lương Gross: 
+```
+Giá trị này có thể nhận giá trị số cũng như dạng phân cách thập phân dấu ',' (20000000 hay 20,000,000)
+```
+Nhập số người phụ thuộc : 
+```
+Giá trị nhận số nguyên >=0
--- thue_thu_nhap_ca_nhan.py
+++ thue_thu_nhap_ca_nhan.py
@@ -30,7 +30,7 @@ def thue_tncn(lương_gross:int,số_người_phụ_thuộc:int):
        elif thu_nhập_tính_thuế  > 80000000 : #Bậc thuế 35%
            thuế = 18150000 + (thu_nhập_tính_thuế-80000000)*35/100 - 4400000*số_người_phụ_thuộc  
    print("-----------------------------------")
-   return bh_tổng,lương_net,thu_nhập_tính_thuế,round(thuế) if thuế > 0 else 0, lương_net - round(thuế) if thuế > 0 else 0 
+   return bh_tổng,lương_net,thu_nhập_tính_thuế,round(thuế) if thuế > 0 else 0, round(lương_net - round(thuế)) if thuế > 0 else 0 
 def input_lương():
    """
    Hàm nhập lương, chỉ nhận giá trị số
@@ -59,6 +59,6 @@ if __name__ == "__main__":
    print(f"lương net là {round(lương_net):,} Đồng")
    print(f"Thu nhập tính thuế là {round(thu_nhập_tính_thuế):,} Đồng")
    print(f'Vậy thuế thu nhập cá nhân bạn cần đóng là : {thuế:,} Đồng')
-   print("-----------------------------------")
    if  còn_lại > 0 :
-       print(f'Sau khi đóng thuế TNCN, bạn còn lại {còn_lại:,} Đồng')
+    print("-----------------------------------") 
+    print(f'Sau khi đóng thuế TNCN, bạn còn lại {còn_lại:,} Đồng')
